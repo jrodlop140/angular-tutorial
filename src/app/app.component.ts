356ca632e10074c554d4b9ec1143aca74089d395
jrodlop140 <jrodlop140@iescarillo.es>
2024-11-17 21:18:31 +0100

    Realiado ejercicio 3 y video 1 de angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasklistComponent } from './components/task/tasklist/tasklist.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent,TasklistComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  nombre = 'Julio Rodríguez';
+}
---
+++ src/app/components/task/tasklist/tasklist.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Task, TaskPriority, TaskStatus } from '../../../models/task.model';
+import { CommonModule } from '@angular/common';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'app-tasklist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tasklist.component.html',
+  styleUrl: './tasklist.component.css'
+})
+export class TasklistComponent implements OnInit{
+  taskList:Task[] = [];
+  
+  ngOnInit(): void {
+    let task1:Task = new Task (1,"Tarea 1", "Descripción Tarea 1",TaskPriority.LOW,TaskStatus.PENDING,new Date("11/1/2024"),new Date("11/18/2024"),false);
+    let task2:Task = new Task (2,"Tarea 2", "Descripción Tarea 2",TaskPriority.HIGH,TaskStatus.IN_PROGRESS,new Date("11/5/2024"),new Date("11/16/2024"),false);
+    let task3:Task = new Task (3,"Tarea 3", "Descripción Tarea 3",TaskPriority.LOW,TaskStatus.IN_PROGRESS,new Date("11/21/2024"),new Date("11/30/2024"),false);
+    let task4:Task = new Task (4,"Tarea 4", "Descripción Tarea 4",TaskPriority.HIGH,TaskStatus.COMPLETED,new Date("11/8/2024"),new Date("11/21/2024"),false);
+    let task5:Task = new Task (5,"Tarea 5", "Descripción Tarea 5",TaskPriority.MEDIUM,TaskStatus.PENDING,new Date("11/10/2024"),new Date("11/30/2024"),false);
+    this.taskList = [task1,task2,task3,task4,task5];
+  }
+}
