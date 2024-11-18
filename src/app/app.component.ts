3212696030b9061ed6c6b16f5916f234cfd0bc8f
jrodlop140 <jrodlop140@iescarillo.es>
2024-11-18 19:57:08 +0100

    Add router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -7,7 +7,7 @@ import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,TasklistComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, TasklistComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'list', component: ListComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: '', redirectTo: '/list', pathMatch: 'full' },
+];
