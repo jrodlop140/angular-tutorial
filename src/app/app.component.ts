9a3285b6477d67824cf8c65cf5fd936d70484e23
jrodlop140 <jrodlop140@iescarillo.es>
2024-11-24 14:02:16 +0100

    Añadido la imagen aleatoria que hicimos en clase y los métodos para añadir funcionalidad al tasklist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, TasklistComponent, RouterLink],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, TasklistComponent, RouterLink,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  nombre = '@juliorodriguez';
+  nombre = " ";
+  imagenAleatoria:string=""
+  numero:number =0;
+
+  //Método que muestra una imagen aleatoria al pulsar unb otón en el html
+  muestraImagen() {
+    let random:number;
+    random = Math.round((Math.random()*1000)+100);
+    this.imagenAleatoria = "https://picsum.photos/200/300?random="+random
+  }
+
 }
--- src/app/components/task/tasklist/tasklist.component.ts
+++ src/app/components/task/tasklist/tasklist.component.ts
@@ -10,15 +10,42 @@ import { take } from 'rxjs';
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.css'
 })
-export class TasklistComponent implements OnInit{
-  taskList:Task[] = [];
-  
+export class TasklistComponent implements OnInit {
+  taskList: Task[] = [];
+
   ngOnInit(): void {
-    let task1:Task = new Task (1,"Tarea 1", "Descripción Tarea 1",TaskPriority.LOW,TaskStatus.PENDING,new Date("11/1/2024"),new Date("11/18/2024"),false);
-    let task2:Task = new Task (2,"Tarea 2", "Descripción Tarea 2",TaskPriority.HIGH,TaskStatus.IN_PROGRESS,new Date("11/5/2024"),new Date("11/16/2024"),false);
-    let task3:Task = new Task (3,"Tarea 3", "Descripción Tarea 3",TaskPriority.LOW,TaskStatus.IN_PROGRESS,new Date("11/21/2024"),new Date("11/30/2024"),false);
-    let task4:Task = new Task (4,"Tarea 4", "Descripción Tarea 4",TaskPriority.HIGH,TaskStatus.COMPLETED,new Date("11/8/2024"),new Date("11/21/2024"),false);
-    let task5:Task = new Task (5,"Tarea 5", "Descripción Tarea 5",TaskPriority.MEDIUM,TaskStatus.PENDING,new Date("11/10/2024"),new Date("11/30/2024"),false);
-    this.taskList = [task1,task2,task3,task4,task5];
+    let task1: Task = new Task(1, "Tarea 1", "Descripción Tarea 1", TaskPriority.LOW, TaskStatus.PENDING, new Date("11/1/2024"), new Date("11/18/2024"), false);
+    let task2: Task = new Task(2, "Tarea 2", "Descripción Tarea 2", TaskPriority.HIGH, TaskStatus.IN_PROGRESS, new Date("11/5/2024"), new Date("11/16/2024"), false);
+    let task3: Task = new Task(3, "Tarea 3", "Descripción Tarea 3", TaskPriority.LOW, TaskStatus.IN_PROGRESS, new Date("11/21/2024"), new Date("11/30/2024"), false);
+    let task4: Task = new Task(4, "Tarea 4", "Descripción Tarea 4", TaskPriority.HIGH, TaskStatus.COMPLETED, new Date("11/8/2024"), new Date("11/21/2024"), false);
+    let task5: Task = new Task(5, "Tarea 5", "Descripción Tarea 5", TaskPriority.MEDIUM, TaskStatus.PENDING, new Date("11/10/2024"), new Date("11/30/2024"), false);
+    this.taskList = [task1, task2, task3, task4, task5];
+  }
+  getTask(taskId: number): Task[] {
+    return this.taskList.filter((tarea: Task) => {
+      return tarea.id == taskId;
+    });
+  }
+
+  raiseTaskPriority(taskId: number) {
+    let tarea: Task = this.getTask(taskId)[0];
+    tarea.raisePriority();
+  }
+
+  lowerTaskPriority(taskId: number) {
+    let tarea: Task = this.getTask(taskId)[0];
+    tarea.lowerPriority();
+  }
+  changeTaskStatus(taskId: number) {
+    let tarea: Task = this.getTask(taskId)[0];
+    tarea.changeStatus();
+  }
+  editTask(taskId: number) {
+    console.log(`Editing Task with identify ${taskId}`);
+  }
+  deleteTask(taskId: number) {
+    this.taskList = this.taskList.filter((tarea: Task) => {
+      return tarea.id != taskId;
+    });
   }
 }
